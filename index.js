8837bb5b00731c36d2a6d712dce13f54f7ae1cbf
ashamary97 <thomasmaryasha1997@gmail.com>
2021-01-17 15:56:31 +0530

    database update

--- index.js
+++ index.js
@@ -9,13 +9,13 @@ var app=Express();
 app.use(bodyparser.json());
 app.use(bodyparser.urlencoded({extended:true}))
 
-mongoose.connect("mongodb+srv://asha:testdb@cluster0.gmzto.mongodb.net/studdb?retryWrites=true&w=majority")
+mongoose.connect("mongodb+srv://asha:testdb@cluster0.gmzto.mongodb.net/studdb?retryWrites=true&w=majority",{useNewUrlParser:true, useUnifiedTopology:true})
 
 app.post("/read", async(req,res)=>{
 try{
 var data=req.body;
 
-var studentData=new studModel(data);
+var studentData=new studModel(req.body);
 
 var result=await studentData.save();
 res.json(result)
